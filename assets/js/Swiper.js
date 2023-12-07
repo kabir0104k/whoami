e915fee6000205010a5e0a4a87dd2df046a1586b
RANJAN <hunt6289@gmail.com>
2023-12-07 09:54:31 +0530

    f

---
+++ assets/js/Swiper.js
@@ -0,0 +1,29 @@
+<head>
+  <!-- ... other head content ... -->
+  <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
+  <style>
+    .swiper-container {
+      width: 100%;
+      height: 100%;
+    }
+
+    .swiper-slide {
+      text-align: center;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+    }
+
+    .certification-logo {
+      width: 100px; /* Adjust the width as needed */
+      height: auto;
+      margin-bottom: 10px;
+    }
+
+    .certification-title {
+      font-size: 18px; /* Adjust the font size as needed */
+      font-weight: bold;
+    }
+  </style>
+</head>
